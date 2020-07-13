0267ed7e8558aacb8b6fab3d0ea12669fdfda7a1
yudaupzt <61223705+yudaupzt@users.noreply.github.com>
2020-07-13 22:30:32 +0700

    Update pocer.php

--- pocer.php
+++ pocer.php
@@ -59,7 +59,7 @@ echo " ===========================\n";
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOJEK20"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0607"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
